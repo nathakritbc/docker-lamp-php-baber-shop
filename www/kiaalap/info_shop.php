e41503672caee72e870080bb7d3e16da07f5db8d
nathakritbc <nathakritbc602@gmail.com>
2023-03-16 21:28:53 +0700

    add

--- www/kiaalap/info_shop.php
+++ www/kiaalap/info_shop.php
@@ -1552,7 +1552,7 @@ $mountThai = dateMonthThaiFormat(date("M"));
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="footer-copy-right">
-                            <p>Copyright © 2018. All rights reserved. Template by <a
+                            <p>Copyright © 2018. All rights reserved. Template by . <a
                                     href="https://colorlib.com/wp/templates/">Colorlib</a></p>
                         </div>
                     </div>
