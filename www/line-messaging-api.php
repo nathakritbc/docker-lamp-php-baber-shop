10b0893b20955ba0073a4ebdf731baf728c02fa9
nathakritbc <nathakritbc602@gmail.com>
2023-04-10 21:19:18 +0700

    a

--- www/line-messaging-api.php
+++ www/line-messaging-api.php
@@ -43,7 +43,7 @@ $data = array(
         ),
         array(
             'type' => 'text',
-            'text' => $urlHttps."?userId=".$userId,
+            'text' => $urlHttps."/login.php?userId=".$userId,
             
         ),
     ),
